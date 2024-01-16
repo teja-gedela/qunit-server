fdc3924c782b84c7a0dfcf3d46023175b3185e8d
teja-gedela <ravitejagedela01@gmail.com>
2024-01-16 13:36:02 +0530

    health check

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.send("health check")
+})
 
 global.makeResult = (statusCode, message) => {
   const result = {
